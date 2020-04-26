d5aee35d5b440ef1501873f0deb89bfd912205cd
flabadens <labadensflorian@gmail.com>
2020-04-26 12:17:36 +0200

    add introduction to css and js files

--- Menus/Menu Slider/menu.js
+++ Menus/Menu Slider/menu.js
@@ -1,3 +1,11 @@
+/*
+  Menu name : Slider
+  Author : geqr
+  Website : www.ma-maison-intelligente.fr
+  Tutorial :
+	Origin : https://codepen.io/raptor5/pen/WxQvgw
+  Last Update : 20200425 - creation
+ */
 $(".menu-opener").click(function(){
   $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
 });
