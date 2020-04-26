8663ee71ccc488d8d7556a9fdfcbd0e5572306ee
flabadens <labadensflorian@gmail.com>
2020-04-26 12:14:05 +0200

    add introduction message to all css & js files

--- Menus/Menu-full-screen/menu.js
+++ Menus/Menu-full-screen/menu.js
@@ -1,16 +1,25 @@
+/*
+  Menu name : Full screen
+  Author : geqr
+  Website : www.ma-maison-intelligente.fr
+  Tutorial : http://ma-maison-intelligente.fr/2020/03/ameliorer-son-design-jeedom-avec-un-menu-full-screen/
+	Origin : https://codepen.io/breadz/pen/zrMRoo
+  Last Update : 20200329 - creation
+ */
+ 
 $('.toggle-menu').click (function(){
   $(this).toggleClass('active');
   $('#menu').toggleClass('open');
 });
 
 
-$("#menu").click(function(event) { 
+$("#menu").click(function(event) {
   $target = $(event.target);
-  if($target.closest('.main-nav').length == 0 && 
+  if($target.closest('.main-nav').length == 0 &&
   $('#menu').hasClass('open')) {
     $("#menu").toggleClass('open');
 	$(".toggle-menu").toggleClass('active');
-  }        
+  }
 });
 
 function closeMenu (){
--- Menus/Menu-indus/menu.js
+++ Menus/Menu-indus/menu.js
@@ -1,3 +1,12 @@
+/*
+  Menu name : Menu style Indus
+  Author : geqr
+  Website : www.ma-maison-intelligente.fr
+  Tutorial : http://ma-maison-intelligente.fr/2020/03/ameliorer-son-design-jeedom-avec-un-menu/
+	Origin : https://codepen.io/AlbertFeynman/pen/mjXeMV
+  Last Update : 20200318 - creation
+ */
+
 const lis = document.querySelectorAll(".buttons-container li");
 const a = document.querySelectorAll(".buttons-container li a");
 
