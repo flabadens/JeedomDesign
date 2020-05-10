24d1d20b395fcd33a4007f012bf26455e29e048c
flabadens <labadensflorian@gmail.com>
2020-05-10 11:19:33 +0200

    add vertical / horizontal slider menu

---
+++ Menus/Menu-slider Vertical/menu.js
@@ -0,0 +1,11 @@
+/*
+Menu name : Slider Vertical
+Author : geqr
+Website : www.ma-maison-intelligente.fr
+Tutorial : http://ma-maison-intelligente.fr/2020/04/un-menu-slider-pour-design-jeedom/
+Origin : https://codepen.io/raptor5/pen/WxQvgw
+Last Update : 20200705 - creation
+ */
+$(".menu-opener").click(function(){
+  $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+});
